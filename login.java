b634d7d3bc61826ac149f63b19f447dc5eade98b
Manjukhandra <khandramanjunath2@gmail.com>
2024-01-31 05:43:33 +0000

    entered pyramid slab in the code

--- login.java
+++ login.java
@@ -1,68 +1,14 @@
-#include <iostream>
-#include <string>
-using namespace std;
+public class Main {
 
-// base class
-class Animal {
+  public static void main(String[] args) {
+    int rows = 5;
 
-   private:
-    string color;
-
-   protected:
-    string type;
-
-   public:
-    void eat() {
-        cout << "I can eat!" << endl;
+    for (int i = rows; i >= 1; --i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
     }
-
-    void sleep() {
-        cout << "I can sleep!" << endl;
-    }
-
-    void setColor(string clr) {
-        color = clr;
-    }
-
-    string getColor() {
-        return color;
-    }
-};
-
-// derived class
-class Dog : public Animal {
-
-   public:
-    void setType(string tp) {
-        type = tp;
-    }
-
-    void displayInfo(string c) {
-        cout << "I am a " << type << endl;
-        cout << "My color is " << c << endl;
-    }
-
-    void bark() {
-        cout << "I can bark! Woof woof!!" << endl;
-    }
-};
-
-int main() {
-    // Create object of the Dog class
-    Dog dog1;
-
-    // Calling members of the base class
-    dog1.eat();
-    dog1.sleep();
-    dog1.setColor("black");
-
-    // Calling member of the derived class
-    dog1.bark();
-    dog1.setType("mammal");
-
-    // Using getColor() of dog1 as argument
-    // getColor() returns string data
-    dog1.displayInfo(dog1.getColor());
-
-    return 0;
+  }
 }
+
