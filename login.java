246cb65952377974bf9ef205aa98abcaaf47a67a
Manjukhandra <khandramanjunath2@gmail.com>
2024-01-31 05:48:10 +0000

    changes in code

--- login.java
+++ login.java
@@ -1,7 +1,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    int rows = 5;
+    int rows = 6;
 
     for (int i = rows; i >= 1; --i) {
       for (int j = 1; j <= i; ++j) {
